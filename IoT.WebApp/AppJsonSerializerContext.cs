f408505a175f3f6badcf99429081dad0f6bcb4e1
Madhon <madhon@madhon.co.uk>
2024-11-07 09:35:39 +0000

    two

--- IoT.WebApp/AppJsonSerializerContext.cs
+++ IoT.WebApp/AppJsonSerializerContext.cs
@@ -10,7 +10,4 @@ using System.Text.Json.Serialization;
     GenerationMode = JsonSourceGenerationMode.Default)
 ]
 [JsonSerializable(typeof(TemperatureResultModel))]
-internal partial class AppJsonSerializerContext : JsonSerializerContext
-{
-    
-}
+internal partial class AppJsonSerializerContext : JsonSerializerContext;
